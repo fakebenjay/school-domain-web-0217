d970f9ee95763737bcffd96eba9e0c65f8e7bbc1
fakebenjay <fakebenjay@gmail.com[color]>
2017-02-08 10:41:05 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :roster, :add_student, :sort
+
+  def initialize(name)
+    @school_name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    @grade = grade
+    if !@roster.keys.include? (@grade)
+      @student_names = Array.new
+      @student_names << student_name
+      @roster[@grade] = @student_names
+    else
+      @roster[@grade] << student_name
+    end
+  end
+
+  def grade(grade)
+    return @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, array|
+      array.sort!
+    end
+  end
+end
